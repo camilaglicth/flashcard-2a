6e8218ac9ce5ee130dc827b73ea1ed0d8984e243
camilaglicth <camila.neves.camargo@escola.pr.gov.br>
2025-05-26 11:23:10 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,14 @@
+criaCartao(
+  'Programação'
+  'O que é Python?', 
+  'O Python é uma linguagem de programação'
+) 
 
+criaCartao(
+  'Geografia',
+  'Qual a capital da França?',
+  'A capital da França é Paris'
+)
+
+criaCartao(
+  
